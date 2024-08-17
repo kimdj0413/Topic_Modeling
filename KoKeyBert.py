ae5937d58e6f1bb3c06bfa24ece52dab9a74a902
Kimdongjun <ejuniya@naver.com>
2024-08-18 00:00:46 +0900

    fe

--- KoKeyBert.py
+++ KoKeyBert.py
@@ -6,39 +6,56 @@ from konlpy.tag import Okt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+import re
 
 doc = """
-         인천 청라 아파트 단지 지하주차장 전기차 화재가 사람들을 공포에 빠뜨렸다. 이번 인천 전기차 화재 피해는 가공할 만하다. 아파트 지하주차장에 주차한 차 수십대가 전기차 화재로 인해 전소됐다. 이뿐만 아니라 화재로 인해 아파트 단지 전기와 수도까지 상당 기간 공급이 중단돼 주민들이 큰 불편을 겪어야 했다. 전기차 화재 초기 진화에 실패하면서 지하주차장에 매설된 아파트 전기 수도 등 기반시설들도 모두 불탔기 때문이다. 이번 화재의 정확한 원인은 수사기관에서 조사 중이지만 전기차 배터리 안전성 문제가 원인으로 지목된다.
-전기차 화재의 위험성과 심각성에 대해 이미 전조증상은 있었다. 이번 화재에 앞서 크고 작은 전기차 화재 사고가 있었고 그때마다 전기차 배터리에서 발생한 불을 끄지 못해 전소하거나 진화에 장시간이 걸리는 문제가 확인됐다. 상당수 전기차 화재가 충전 중에 발생하고 있다는 점 때문에 이에 대한 예방책도 마련돼야 한다는 주장도 제기돼 왔다.
-경기일보도 로컬이슈 리포트(2023년 3월17일자 1·3면) 등을 통해 전기차 화재의 위험성과 대책 마련이 시급하다고 여러 차례 경고한 터라 이번 인천 청라 아파트 전기차 화재가 더 안타깝게 다가온다.
-경기일보가 주목한 부분은 전기차 충전시설이 아파트 등 지하주차장에 급속도로 설치되고 있다는 점이었다. 안전대책이 없는 상황에서 폐쇄적인 지하주차장에 전기충전시설을 설치할 때 전기차 화재 발생 시 피해는 더 클 수밖에 없다. 그러나 친환경 전기차 활성화 정책을 펴고 있는 정부는 전기차 이용자들의 편의를 위해 아파트 단지 지하 등에 전기차 충전시설 설치를 권장하고 있다.
-그러나 확인 결과 안전대책은 역시 미흡했다. 당시 기자들이 아파트 단지 등에 설치된 전기차 충전시설 몇 곳을 돌아봤는데 화재 발생 시 속수무책일 수밖에 없었다. 지하주차장 특성상 화재 발생 시 소방차가 진입할 수 없는 구조이거나 그 흔한 분말소화기조차 비치하지 않은 곳도 있었다. 전기차 배터리 화재는 일반 소화기로는 사실상 진압할 수 없다. 전기차 배터리에서 열폭주를 일으키며 피해는 걷잡을 수 없이 커진다. 전문가들은 전기차 안전 관련 법 제도 강화와 화재 발생 시 신속 진화할 수 있는 시스템 개발 등을 제안했다.
-당시 경기일보 기사를 보고 부천시에서 수원 본사까지 찾아 문의하는 독자가 있을 정도로 전기차 화재에 대한 시민들의 공포는 퍼져 있는 상황이었다.
-인천 청라 전기차 화재 이후 전기차에 대한 불안감은 더욱 확산되는 양상이다. 전기차 출입을 금지하는 아파트 단지가 등장했다. 정부와 지자체가 뒤늦게 대책 마련에 분주하다. 전기차 생산업체들은 배터리 제조사를 공개하기 시작했다.
-인천 청라 전기차 화재 사고 전 전기차 화재의 위험성과 심각성은 모두 알고 있었다. 이에 대한 경고음도 이미 수차례 울린 상황이다. 그러나 우물쭈물하는 사이 대형 사고는 여지없이 발생했다. 언제까지 큰 희생을 치른 뒤 대책이 마련되는 상황을 경험해야 하는지 시민들은 답답하기만 하다.
+        모빌리티 플랫폼 쏘카가 적자를 기록했다. 체질 개선을 위한 투자를 우선했기 때문이다. 하반기부터는 흑자 전환을 위한 움직임을 본격화하겠다는 구상이다. 
+
+쏘카는 올해 2분기 연결 기준 영업손실이 약 67억원으로 잠정 집계됐다고 13일 공시했다. 지난해 2분기 영업이익은 16억원이었으나 이번에는 적자를 냈다.
+
+2분기 매출액은 약 1018억원으로 전년 동기 1039억원 대비 약 2.0% 감소했다. 당기순손실은 약 116억원을 기록했다.
+
+쏘카는 차량 이용주기 확대를 위해 중고차량 매각을 최소화하면서 특히 2분기 중고차 판매 매출이 지난해 172억원에서 올해 10억원으로 약 94.2% 감소했다고 설명했다. 아울러 카셰어링 부문 수요 창출, 플랫폼 부문 투자, 마케팅 확대로 간접비가 1년 새 약 33.4% 늘면서 영업 적자 요인으로 작용했다.
+
+단기 카셰어링과 쏘카플랜(장기)으로 이루어진 카셰어링 부문에서는 2분기에만 914억원의 매출을 기록했다. 특히 단기 카세어링의 매출총이익률(GPM)은 지난해 2분기 17.1%에서 올해 18.9%로 올랐다. 
+
+운1개월 이상 대여상품 쏘카플랜 GPM은 지난해 2분기 26.7%에서 올해 마이너스(-)8.1%로 급감했다. 상반기 운영 차량을 3배 확대하며 공격적인 프로모션을 전개한 영향이 컸다. 다만 올해 1분기와 견주면 7.2%포인트 상승한 수치다. 
+
+쏘카는 쏘카플랜 GPM이 지난 6월부터 반등해 3분기부터는 다시 분기 전체 '플러스(+)'로 전환할 것으로 전망하고 있다. 차량·이용자 생애주기이익(LTV) 증대를 위해 지난해 하반기부터 시작한 쏘카 2.0 전략이 본궤도에 진입하며 매출총이익률도 본격 개선세에 접어들었다는 설명이다.
+
+공유 전기자전거 '일레클', 온라인 주차 플랫폼 '모두의주차장', 숙박 등 플랫폼 부문 총거래액(GMV)은 서비스 라인업 확장에 따라 2분기 255억원을 기록했다. 전년 동기 대비 약 27.2%(55억원) 증가했다. 일레클의 일회성 기기판매 거래액을 제외하면 약 47.8% 늘었다. 같은 기간 월간 앱 방문자수(MUV)는 약 1.2배 증가한 152만명을 기록했다.
+
+쏘카는 부름·편도 등 고부가 서비스 확대, 네이버 채널링 효과 등으로 3분기 흑자 달성을 노리고 있다. 2분기까지 쏘카 2.0 전략을 위한 전략적 투자가 일단락되고 하반기부터는 마케팅비용도 줄어들어 안정적인 수익 달성에 기여할 것이라고 설명했다.
+
+박재욱 쏘카 대표는 "지난 1년간 전개해 온 쏘카 2.0 전략을 통해 카셰어링 시장의 성장 잠재력과 시장을 리드하는 쏘카의 능력을 입증했다"며 "3분기부터 안정적으로 이익을 창출할 수 있는 전사 차원의 체질 개선을 확신한다"고 말했다.
       """
+split_doc = re.split(r'(\s*[a-zA-Z]+(?:[^\w\s]+[a-zA-Z]+)*\s*)', doc)
+
+# 불필요한 공백 제거
+split_doc = [part.strip() for part in split_doc if part.strip()]
 
 ##    형태소 분석
 okt = Okt()
-nouns = okt.nouns(doc)
+nouns=[]
+for sentence in split_doc:
+    sen = okt.nouns(sentence)
+    if len(sen) != 0:
+        nouns.extend(sen)
+    else:
+        nouns.append(sentence)
 
 text = ' '.join(nouns)
 
 ##    N Gram 생성
-n_gram_range = (2,3)
+n_gram_range = (1,2)
 
 count = CountVectorizer(ngram_range=n_gram_range).fit([text])
 candidates = count.get_feature_names_out()
-
-print('trigram 개수 :',len(candidates))
-print('trigram 다섯개만 출력 :',candidates[:5])
-
+print(candidates)
 ##    SBERT 사용해 수치화
 model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
 doc_embedding = model.encode([doc])
 candidate_embeddings = model.encode(candidates)
-print(f'doc_embedding : {doc_embedding}')
-print(f'\ncandidate_embedding : {candidate_embeddings}')
 
 ##    상위 5개 출력
 top_n = 5
@@ -47,40 +64,65 @@ keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
 
 print(keywords)
 
-##    MMR 기법 사용해 다양성 최대화
-def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
-
-    # 문서와 각 키워드들 간의 유사도가 적혀있는 리스트
-    word_doc_similarity = cosine_similarity(candidate_embeddings, doc_embedding)
+def max_sum_sim(doc_embedding, candidate_embeddings, words, top_n, nr_candidates):
+    # 문서와 각 키워드들 간의 유사도
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
 
     # 각 키워드들 간의 유사도
-    word_similarity = cosine_similarity(candidate_embeddings)
-    print(f'word_similarity : {word_similarity}')
+    distances_candidates = cosine_similarity(candidate_embeddings, 
+                                            candidate_embeddings)
 
-    # 문서와 가장 높은 유사도를 가진 키워드의 인덱스를 추출.
-    # 만약, 2번 문서가 가장 유사도가 높았다면
-    # keywords_idx = [2]
-    keywords_idx = [np.argmax(word_doc_similarity)]
+    # 코사인 유사도에 기반하여 키워드들 중 상위 top_n개의 단어를 pick.
+    words_idx = list(distances.argsort()[0][-nr_candidates:])
+    words_vals = [candidates[index] for index in words_idx]
+    distances_candidates = distances_candidates[np.ix_(words_idx, words_idx)]
 
-    # 가장 높은 유사도를 가진 키워드의 인덱스를 제외한 문서의 인덱스들
-    # 만약, 2번 문서가 가장 유사도가 높았다면
-    # ==> candidates_idx = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10 ... 중략 ...]
-    candidates_idx = [i for i in range(len(words)) if i != keywords_idx[0]]
+    # 각 키워드들 중에서 가장 덜 유사한 키워드들간의 조합을 계산
+    min_sim = np.inf
+    candidate = None
+    for combination in itertools.combinations(range(len(words_idx)), top_n):
+        sim = sum([distances_candidates[i][j] for i in combination for j in combination if i != j])
+        if sim < min_sim:
+            candidate = combination
+            min_sim = sim
 
-    # 최고의 키워드는 이미 추출했으므로 top_n-1번만큼 아래를 반복.
-    # ex) top_n = 5라면, 아래의 loop는 4번 반복됨.
-    for _ in range(top_n - 1):
-        candidate_similarities = word_doc_similarity[candidates_idx, :]
-        target_similarities = np.max(word_similarity[candidates_idx][:, keywords_idx], axis=1)
+    return [words_vals[idx] for idx in candidate]
+print(max_sum_sim(doc_embedding, candidate_embeddings, candidates, top_n=5, nr_candidates=10))
+print(max_sum_sim(doc_embedding, candidate_embeddings, candidates, top_n=5, nr_candidates=30))
+# ##    MMR 기법 사용해 다양성 최대화
+# def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
 
-        # MMR을 계산
-        mmr = (1-diversity) * candidate_similarities - diversity * target_similarities.reshape(-1, 1)
-        mmr_idx = candidates_idx[np.argmax(mmr)]
+#     # 문서와 각 키워드들 간의 유사도가 적혀있는 리스트
+#     word_doc_similarity = cosine_similarity(candidate_embeddings, doc_embedding)
 
-        # keywords & candidates를 업데이트
-        keywords_idx.append(mmr_idx)
-        candidates_idx.remove(mmr_idx)
+#     # 각 키워드들 간의 유사도
+#     word_similarity = cosine_similarity(candidate_embeddings)
+#     print(f'word_similarity : {word_similarity}')
 
-    return [words[idx] for idx in keywords_idx]
+#     # 문서와 가장 높은 유사도를 가진 키워드의 인덱스를 추출.
+#     # 만약, 2번 문서가 가장 유사도가 높았다면
+#     # keywords_idx = [2]
+#     keywords_idx = [np.argmax(word_doc_similarity)]
+
+#     # 가장 높은 유사도를 가진 키워드의 인덱스를 제외한 문서의 인덱스들
+#     # 만약, 2번 문서가 가장 유사도가 높았다면
+#     # ==> candidates_idx = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10 ... 중략 ...]
+#     candidates_idx = [i for i in range(len(words)) if i != keywords_idx[0]]
+
+#     # 최고의 키워드는 이미 추출했으므로 top_n-1번만큼 아래를 반복.
+#     # ex) top_n = 5라면, 아래의 loop는 4번 반복됨.
+#     for _ in range(top_n - 1):
+#         candidate_similarities = word_doc_similarity[candidates_idx, :]
+#         target_similarities = np.max(word_similarity[candidates_idx][:, keywords_idx], axis=1)
+
+#         # MMR을 계산
+#         mmr = (1-diversity) * candidate_similarities - diversity * target_similarities.reshape(-1, 1)
+#         mmr_idx = candidates_idx[np.argmax(mmr)]
+
+#         # keywords & candidates를 업데이트
+#         keywords_idx.append(mmr_idx)
+#         candidates_idx.remove(mmr_idx)
+
+#     return [words[idx] for idx in keywords_idx]
   
-print(mmr(doc_embedding, candidate_embeddings, candidates, top_n=5, diversity=0.5))
+# print(mmr(doc_embedding, candidate_embeddings, candidates, top_n=5, diversity=0.5))
--- ThemeRecommend.py
+++ ThemeRecommend.py
@@ -6,20 +6,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 import os
 import pickle
+import re
 
 ##      뉴스기사
 article = """
-16일 국내 최대 중고차 거래 플랫폼 엔카에 따르면 이날 기준 이 업체 웹사이트에 매매 등록된 메르세데스-벤츠 EQE 350+모델은 총 85대다. 이들 차량은 지난달만 해도 연식(2022년~2024년식)에 따라 6000만대 중반부터 7000만원대까지 중고 시세가 형성했는데 청라 화재 사고 이후에는 6000만원대 초반으로 급락했다. 이주 들어서는 급기야 5900만원대를 깬 5850만원짜리 매물도 등장했다.
+정부가 내수가 살아날 조짐을 보인다는 경기 진단을 넉 달째 이어갔다. 길어지는 내수 부진을 이유로 국책 연구기관은 올해 성장률 전망치까지 낮춘 만큼 내수 회복세를 낙관하긴 이르다는 지적이 나온다.
 
-업계 한 관계자는 “통상 자동차 감가상각은 신차 출고 이후 1년~3년 사이에 높게 이뤄지고, 감가 폭은 연 10% 규모”라며 “특히 전기차는 내연기관차에 비해 시중에 풀린 물량이 많지 않아 감가가 더 크게 이뤄지고 시세도 수요에 따라 요동치는 편인데 이번에 전기차 화재 사고로 가격 하락폭이 더 커졌다”고 설명했다. 벤츠 EQE 350+ 신차 출고가가 1억380만원인 점을 고려하면 2022년식 차량이 2년 만에 40％ 넘게 가격이 빠졌다.
+기획재정부는 16일 ‘최근 경제동향(그린북)’ 8월호에서 “최근 우리 경제는 견조한 수출·제조업 호조세에 설비투자 중심의 완만한 내수 회복 조짐을 보이며 경기 회복 흐름이 지속되는 모습”이라고 평가했다. 올 5월 ‘내수 회복 조짐’을 처음 꺼낸 정부가 넉 달째 이런 시각을 유지한 것이다.
 
-화재 사고 이후 중고차 시장에선 벤츠 전기차뿐만 아니라 다른 브랜드의 전기차 차주들도 차량을 처분하려는 움직임이 늘고 있다. 국내 최대 직영중고차 업체 케이카(K Car)에 따르면 지난주(1~7일) ‘내차 팔기 홈 서비스’에 등록된 전기차 접수량은 직전 주(지난달 25∼31일) 대비 184% 급증했다. 차량 대수는 공개하지 않았지만, 이중 벤츠 EQE 시리즈 모델이 10% 정도 차지했다. 케이카 관계자는 “이주(8~14일) 들어 등록된 접수량은 전주(1~7일) 물량의 90% 수준”이라며 “급격히 떨어진 전기차 가격이 진정될 때까지 기다리려는 이들이 늘면서 매물 접수도 일단 진정된 상태”고 설명했다.
+김귀범 기재부 경제분석과장은 “실질임금이 두 달 연속 상승했고 방한 관광객과 카드 매출액 속보 지표도 연초 이후 개선 흐름을 보이고 있다. 소비자심리지수도 2020년 4월 이후 가장 높은 수준”이라고 설명했다.
 
-전기차를 처분하려는 이들은 늘었지만, 이번 화재 사고로 중고차 업계에선 전기차 매입을 꺼려하는 분위기도 빠르게 확산중이다. 서울의 한 중고차 딜러는 벤츠 EQE 모델의 경우 아예 ‘매입 거부’를 공개적으로 밝힌 곳도 등장했다. 상대적으로 가격이 비싼 수입차의 경우 실내 주차장에 보관하는 경우가 많은데, 이번에 화재가 난 차량도 충전 중이 아닌 상태에서 불이 났던 만큼 차량을 보관하는 것도 우려되기 때문이다.
+하지만 정부의 이런 시각은 한국개발연구원(KDI) 등 외부의 시각과는 온도 차가 있다. KDI는 앞서 7일 ‘경제전망 수정’에서 올해 성장률 전망치를 2.6%에서 2.5%로 0.1%포인트 낮춰 잡았다. 당초 예상보다 내수가 미약한 수준에 그치면서 경기 회복의 발목을 잡고 있다는 판단이었다. KDI의 전망치는 기재부가 6월 전망한 올해 성장률(2.6%)보다 낮다.
 
-자동차 업계는 가뜩이나 침체한 전기차 시장이 이번 화재 사고 이후 수요 둔화가 더욱 장기화할까 우려하는 분위기다. 업계 관계자는 “완성차업계가 전기차 캐즘(일시적 수요 둔화)에 이어 포비아(공포증)까지 퍼지면서 신차 판매에 당장 제동이 걸린 상황”이라며 “전기차 프로모션과 함께 배터리 정보도 자발적으로 공개하며 소비자 불안을 해소하는 데 총력을 기울이고 있지만 수요를 회복하기엔 쉽지 않은 상황”이라고 말했다.
+실제 내수 상황을 보여주는 일부 지표는 부진을 벗어나지 못하고 있다. 상품 소비를 보여주는 소매판매는 1년 전보다 3.6% 감소했다. 최근에는 특히 백화점과 할인점의 매출이 줄줄이 쪼그라들면서 소비 지표가 꺾이고 있다. 지난달 백화점의 국내 카드 승인액은 1년 전보다 1.4% 줄며 두 달 연속 감소세를 이어갔고, 할인점 매출액은 3.3% 감소해 6월보다 감소 폭이 더 커졌다. 설비투자 역시 1년 전보다 2.7% 줄었다.
 
-한편, 국토교통부 자동차리콜센터가 공개한 ‘제작사별 차명별 배터리 제조사’ 현황에 따르면 이날 기준 국내에서 전기차를 판매하는 업체 중 현대차와 기아, 르노코리아, KGM, 벤츠, BMW, 볼보, 스텔란티스, 포르쉐, 폴스타, 폭스바겐, 토요타, 테슬라 등 13개 완성차 브랜드가 배터리 제조사 정보를 공개한 상태다.
+7월 물가상승률 역시 6월(2.4%)보다 소폭 확대된 2.6%였다. 집중호우와 유가 상승 등의 영향으로 농산물, 석유류 물가가 비싸진 탓이다. 건설경기 악화에 건설투자 역시 4.6% 줄었다. 이 때문에 정부가 내놓은 내수 진단이 서민들이 느끼는 체감 경기를 반영하지 못한다는 지적도 나온다.
+
+하준경 한양대 경제학부 교수는 “물가가 전보다 안정되긴 했지만 최근 2년간 실질소득이 뒷걸음질한 상황이라 내수 회복의 여건이 되지 못하고 있다”며 “내수 회복세를 낙관하기는 이르다”고 말했다. 그는 “내수가 1분기(1∼3월) 반짝 회복세를 보인 건 정부가 재정을 당겨 쓴 영향도 있다. 이제는 그럴 재정도 남아 있지 않다”고 덧붙였다.
 """
 
 ##      형태소 분석 및 N Gram 생성
@@ -27,7 +30,7 @@ okt = Okt()
 nouns = okt.nouns(article)
 text = ' '.join(nouns)
 
-n_gram_range = (1,2)
+n_gram_range = (2,3)
 count = CountVectorizer(ngram_range=n_gram_range).fit([text])
 candidates = count.get_feature_names_out()
 
@@ -52,7 +55,7 @@ def mmr(doc_embedding, candidate_embeddings, words, top_n, diversity):
 
     return [words[idx] for idx in keywords_idx]
   
-keyword = mmr(doc_embedding, candidate_embeddings, candidates, top_n=3, diversity=0.7)
+keyword = mmr(doc_embedding, candidate_embeddings, candidates, top_n=3, diversity=0.1)
 print(keyword)
 
 # # 특정 디렉토리 경로
@@ -68,33 +71,45 @@ print(keyword)
 # name_list = [name.replace('_embedding.pkl', '') for name in name_list]
 # print(name_list)
 
-# 특정 디렉토리 경로
-directory_path = 'C:/Topic_Modeling/embeddings'
+# # 특정 디렉토리 경로
+# directory_path = 'C:/Topic_Modeling/embeddings'
 
-# 디렉토리의 모든 파일 이름 불러오기
-doc_embedding_list = []
-name_list = []
-file_names = os.listdir(directory_path)
-for file in file_names:
-    name_list.append(file)
-    with open(f'C:/Topic_Modeling/embeddings/{file}', 'rb') as file:
-        doc_embedding = pickle.load(file)
-        doc_embedding_list.append(doc_embedding)
+# # 디렉토리의 모든 파일 이름 불러오기
+# doc_embedding_list = []
+# name_list = []
+# file_names = os.listdir(directory_path)
+# for file in file_names:
+#     name_list.append(file)
 
-cosine_score=[]
-candidate_embeddings = model.encode(keyword)
-for doc_embedding in doc_embedding_list:
-    word_doc_similarity = cosine_similarity(candidate_embeddings, doc_embedding)
-    sum=0
-    for sim in word_doc_similarity:
-        sum += sim
-    cosine_score.append(sum/len(word_doc_similarity))
+#     with open(f'C:/Topic_Modeling/embeddings/{file}', 'rb') as file:
+#         doc_embedding = pickle.load(file)
+#         doc_embedding_list.append(doc_embedding)
 
-indexed_numbers = [(value, index) for index, value in enumerate(cosine_score)]
-sorted_numbers = sorted(indexed_numbers)
-smallest_three = sorted_numbers[10:20]
-smallest_values = [value for value, index in smallest_three]
-smallest_indices = [index for value, index in smallest_three]
+# cosine_score=[]
+# candidate_embeddings = model.encode(keyword)
+# for doc_embedding in doc_embedding_list:
+#     word_doc_similarity = cosine_similarity(candidate_embeddings, doc_embedding)
+#     sum=0
+#     for sim in word_doc_similarity:
+#         sum += sim
+#     cosine_score.append(sum/len(word_doc_similarity))
 
-for index in smallest_indices:
-    print(name_list[index])
+# indexed_numbers = [(value, index) for index, value in enumerate(cosine_score)]
+# sorted_numbers = sorted(indexed_numbers)
+# smallest_three = sorted_numbers[10:20]
+# smallest_values = [value for value, index in smallest_three]
+# smallest_indices = [index for value, index in smallest_three]
+
+# for index in smallest_indices:
+#     print(name_list[index])
+
+with open('C:/Topic_Modeling/theme_dict.pkl', 'rb') as f:
+    loaded_dict = pickle.load(f)
+
+keys = loaded_dict.keys()
+keys_list = list(keys)
+
+new_keys_list = [re.sub(r'[^a-zA-Z0-9가-힣\s]', ' ', key) for key in keys_list]
+
+key_embeddings = model.encode(new_keys_list)
+print(mmr(doc_embedding, key_embeddings, keys_list, top_n=3, diversity=0.1))
